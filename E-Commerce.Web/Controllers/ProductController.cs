ffe966baee90f278e313ea7ff1e94c8e4ccefd45
youssefemaad <eyoussef228@gmail.com>
2025-04-18 20:51:11 +0200

    Onion Architecture

---
+++ E-Commerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        
+    }
+}
