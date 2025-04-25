b84d4df80ec81e40549f820bbf38fd91bd531414
youssefemaad <eyoussef228@gmail.com>
2025-04-25 22:19:02 +0300

    Cutom Exception MiddleWare

--- Infrastructure/Presistence/DataSeeding.cs
+++ Infrastructure/Presistence/DataSeeding.cs
@@ -12,14 +12,15 @@ namespace Presistence
         {
             try
             {
+                // Apply pending migrations if any
                 if ((await _dbContext.Database.GetPendingMigrationsAsync()).Any())
                 {
                     await _dbContext.Database.MigrateAsync();
                 }
 
+                // Seed ProductBrands
                 if (!_dbContext.ProductBrands.Any())
                 {
-                    // var ProductBrandsData = await File.ReadAllTextAsync(@"..\Data\DataSeed\brands.json");
                     var ProductBrandsData = File.OpenRead(@"..\Infrastructure\Presistence\Data\DataSeed\brands.json");
                     var ProductBrands = await JsonSerializer.DeserializeAsync<List<ProductBrand>>(ProductBrandsData);
 
@@ -29,6 +30,7 @@ namespace Presistence
                     }
                 }
 
+                // Seed ProductTypes
                 if (!_dbContext.ProductTypes.Any())
                 {
                     var ProductTypesData = File.OpenRead(@"..\Infrastructure\Presistence\Data\DataSeed\types.json");
@@ -39,7 +41,7 @@ namespace Presistence
                         await _dbContext.ProductTypes.AddRangeAsync(ProductTypes);
                     }
                 }
-
+                // Seed Products
                 if (!_dbContext.Products.Any())
                 {
                     var ProductsData = File.OpenRead(@"..\Infrastructure\Presistence\Data\DataSeed\products.json");
