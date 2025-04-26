c04b0b65bccd60f9c2020e9714d70ae23e069e89
youssefemaad <eyoussef228@gmail.com>
2025-04-26 09:23:05 +0300

    Basket Module - Controller

--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -4,9 +4,12 @@ using ServiceAbstraction;
 
 namespace Service
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository basketRepository) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyProductService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
+        private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(basketRepository, _mapper));
         public IProductService ProductService => _LazyProductService.Value;
+
+        public IBasketService basketService => _LazyBasketService.Value;
     }
 }
